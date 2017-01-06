07f767809219b852a0d65b08a9475ca296ef2ef2
Marsu5 <marcell.schuh@gmail.com>
2017-01-06 14:52:39 +0100

    Initial Commited Suicide

---
+++ desktop/src/hu/tokingame/silverdart/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package hu.tokingame.silverdart.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import hu.tokingame.silverdart.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
